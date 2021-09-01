ba2fa4bc86f5cac257db30e46f00539b4fe00053
chaitra845 <chaitrachytra@gmail.com>
2021-09-01 19:38:31 +0530

    Added code to retrieve data from database

---
+++ src/main/java/JDBC_Connectivity/PayrollService_JDBC/Employee.java
@@ -0,0 +1,37 @@
+package JDBC_Connectivity.PayrollService_JDBC;
+public class Employee {
+    public int Id;
+    public String firstName;
+    public String lastName;
+    public float basicPay;
+
+    public float getBasicPay() {
+        return basicPay;
+    }
+    public void setBasicPay(float basicPay) {
+        this.basicPay = basicPay;
+    }
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        Id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    @Override
+    public String toString() {
+        return "Employee [Id=" + Id + ", firstName=" + firstName + ", lastName=" + lastName + ", basicPay=" + basicPay
+                + "]";
+    }
+}
--- src/main/java/JDBC_Connectivity/PayrollService_JDBC/EmployeeRepo.java
+++ src/main/java/JDBC_Connectivity/PayrollService_JDBC/EmployeeRepo.java
@@ -2,12 +2,15 @@ package JDBC_Connectivity.PayrollService_JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeRepo {
 
-    public void insertRecord(EmployeeDetails details) throws ClassNotFoundException, SQLException {
+    public void insertRecord(Employee details) throws ClassNotFoundException, SQLException {
         Connection connection = null;
         Statement statement = null;
         try {
@@ -38,4 +41,57 @@ public class EmployeeRepo {
             }
         }
     }
+
+    public List<Employee> findAll() throws SQLException {
+        List<Employee> details=new ArrayList<>();
+
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            //Step1: Load & Register Driver Class
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver ());
+
+            //Step2: Establish a MySql Connection
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_payroll_service", "root", "root");
+
+            //Step3: Create Statement
+            statement = connection.createStatement();
+
+            //Step4: Execute Query
+            String query =" select * from employee_payroll";
+            ResultSet resultset = statement.executeQuery(query);
+
+            while(resultset.next()) {
+                Employee info = new Employee();
+
+                int id=resultset.getInt(1);
+                info.setId(id);
+
+                String name = resultset.getString(2);
+                info.setFirstName(name);
+
+                String lastname = resultset.getString(3);
+                info.setLastName(lastname);
+
+                float pay =resultset.getFloat(4);
+                info.setBasicPay(pay);
+
+                details.add(info);
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+
+            if(connection != null) {
+                statement.close();
+            }
+            if(statement != null) {
+                connection.close();
+            }
+        }
+        return details;
+    }
+
 }
